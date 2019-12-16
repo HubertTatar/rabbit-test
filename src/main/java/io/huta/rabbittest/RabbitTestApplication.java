dc41876520e214527f7cf5ef2d69c7459bf8b72d
Hubert Tatar <hutatar@gmail.com>
2019-12-16 21:39:59 +0100

    initial

---
+++ src/main/java/io/huta/rabbittest/RabbitTestApplication.java
@@ -0,0 +1,15 @@
+package io.huta.rabbittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class RabbitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/huta/rabbittest/RabbitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.huta.rabbittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
